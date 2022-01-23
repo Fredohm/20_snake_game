aa69b6a53368648b15a388d70f24ddc4bc4742cb
Fredohm <fredwaucquez@hotmail.fr>
2022-01-23 11:36:16 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Snake game
+from turtle import Turtle, Screen
+
+# Create a snake body
+
+# Move the snake
+
+# Control the snake
+
+# Detect collision with food
+
+# Create a scoreboard
+
+# Detect collision with a wall
+
+# Detect collision with the tail of the snake
+
