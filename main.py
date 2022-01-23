c73267e77906a8a637e526a1c2ace1fabdd1d768
Fredohm <fredwaucquez@hotmail.fr>
2022-01-23 14:41:30 +0100

    create a snake body

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 
 # Create a snake body
+snake = []
+
+x_pos = 0.0
+for _ in range(3):
+    turtle = Turtle(shape="square")
+    turtle.color("white")
+    turtle.penup()
+    turtle.setx(x_pos)
+    x_pos += -20.0
+    snake.append(turtle)
 
 # Move the snake
 
