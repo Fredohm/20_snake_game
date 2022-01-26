a4f01ae067cc590fc63f7aa9b3cd8cf731acf96f
Fredohm <fredwaucquez@hotmail.fr>
2022-01-26 10:02:05 +0100

    Create snake class and move function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Snake game
-from turtle import Turtle, Screen
+from turtle import Screen
+from snake import Snake
 import time
 
 screen = Screen()
@@ -8,23 +9,7 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-
-# Create a snake body
-def create_snake():
-    segments = []
-
-    x_pos = 0.0
-    for _ in range(3):
-        new_segment = Turtle(shape="square")
-        new_segment.color("white")
-        new_segment.penup()
-        new_segment.setx(x_pos)
-        x_pos += -20.0
-        segments.append(new_segment)
-    return segments
-
-
-# Move the snake
+snake = Snake()
 
 # Control the snake
 
@@ -36,14 +21,12 @@ def create_snake():
 
 # Detect collision with the tail of the snake
 
-# game on
+
 game_is_on = True
-snake = create_snake()
 while game_is_on:
     screen.update()
-    time.sleep(0.2)
-    for segment in snake:
-        segment.forward(10)
+    time.sleep(0.1)
 
+    snake.move()
 
 screen.exitonclick()
